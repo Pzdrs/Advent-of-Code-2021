42a91b20281dafec4a6d3bc20aa1dc22fca0934b
Petr (Desktop) <petrbohac3@seznam.cz>
2021-12-17 15:47:46 +0100

    02 - done

--- src/AdventOfCode.java
+++ src/AdventOfCode.java
@@ -1,10 +1,30 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class AdventOfCode {
     public static void main(String[] args) {
-
+        int[] data = getData();
+        if (data != null) {
+            int sumsIncrease = 0;
+            int last = 0;
+            for (int i = 0; i < data.length; i++) {
+                try {
+                    int[] window = new int[]{data[i], data[i + 1], data[i + 2]};
+                    int sum = Arrays.stream(window).sum();
+                    if (i == 0) {
+                        last = sum;
+                        continue;
+                    }
+                    if (sum > last) sumsIncrease++;
+                    last = sum;
+                } catch (ArrayIndexOutOfBoundsException exception) {
+                    break;
+                }
+            }
+            System.out.println(sumsIncrease);
+        }
     }
 
     public static int[] getData() {
