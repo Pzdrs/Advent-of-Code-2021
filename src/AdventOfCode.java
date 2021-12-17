337ce8612547d7ca5bb7b172fbec007f7faefb86
Petr (Desktop) <petrbohac3@seznam.cz>
2021-12-17 15:50:26 +0100

    Fixed main

--- src/AdventOfCode.java
+++ src/AdventOfCode.java
@@ -1,18 +1,5 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class AdventOfCode {
     public static void main(String[] args) {
 
-    }
-
-    public static int[] getData() {
-        try {
-            return Files.lines(Path.of("src/data.txt")).mapToInt(Integer::parseInt).toArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
