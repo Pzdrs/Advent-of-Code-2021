4f83e892526e3d8514cdcfeca60c084e57760802
Petr (Desktop) <petrbohac3@seznam.cz>
2021-12-17 15:30:08 +0100

    01 - done

--- src/AdventOfCode.java
+++ src/AdventOfCode.java
@@ -1,5 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class AdventOfCode {
     public static void main(String[] args) {
+        int[] data = getData();
+        if (data != null) {
+            int measurementsLargerThanPrevious = 0;
+            int last = 0;
+            for (int i = 0; i < data.length; i++) {
+                if (i == 0) {
+                    last = data[i];
+                    continue;
+                }
+                if (data[i] > last) measurementsLargerThanPrevious++;
+                last = data[i];
+            }
+            System.out.println(measurementsLargerThanPrevious);
+        }
+    }
 
+    public static int[] getData() {
+        try {
+            return Files.lines(Path.of("src/data.txt")).mapToInt(Integer::parseInt).toArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
