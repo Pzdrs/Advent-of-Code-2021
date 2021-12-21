339d2ed2a17b329e64e78fadf8753ef5ccf0ab52
Petr (Desktop) <petrbohac3@seznam.cz>
2021-12-21 19:51:01 +0100

    07 - done

--- src/AdventOfCode.java
+++ src/AdventOfCode.java
@@ -1,5 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class AdventOfCode {
     public static void main(String[] args) {
+        int[] crabs = loadData();
+        List<Integer> fuel = new ArrayList<>();
+        System.out.println(Arrays.toString(crabs));
 
+        for (int i = Arrays.stream(crabs).min().getAsInt(); i < Arrays.stream(crabs).max().getAsInt(); i++) {
+            int fuelNeeded = 0;
+            for (int crab : crabs) {
+                fuelNeeded += Math.abs(crab - i);
+            }
+            fuel.add(fuelNeeded);
+        }
+        System.out.println(fuel.stream().min(Integer::compareTo).get());
+    }
+
+    public static int[] loadData() {
+        List<Integer> list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new FileReader("src/data.txt"));
+            for (String s : scanner.next().split(",")) {
+                list.add(Integer.parseInt(s));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
